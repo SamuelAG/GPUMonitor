23731967eb3e36c5732ef39602e2722af15d449e
Samuel Alves <samuel.alvesg@hotmail.com>
2024-06-27 21:39:56 -0300

    Adds .qrc resource file

--- app/main.cpp
+++ app/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/GPUMonitor/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
